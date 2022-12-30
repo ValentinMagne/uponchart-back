5a9850049530819f9256acb1f4b73250998fd1cf
Valentin Magne <valentin.magne@orange.com>
2022-12-30 20:20:55 +0100

    Added CORS config

--- src/main/java/com/costardstudio/uponchart/UponchartApplication.java
+++ src/main/java/com/costardstudio/uponchart/UponchartApplication.java
@@ -2,12 +2,25 @@ package com.costardstudio.uponchart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class UponchartApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UponchartApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UponchartApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
+    }
 
 }
