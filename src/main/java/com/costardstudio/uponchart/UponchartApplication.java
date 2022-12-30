94bc1da28134cf482e4d00f0fa03b21914a5e31c
Valentin Magne <valentin.magne@orange.com>
2022-12-30 15:54:19 +0100

    Initial commit

---
+++ src/main/java/com/costardstudio/uponchart/UponchartApplication.java
@@ -0,0 +1,13 @@
+package com.costardstudio.uponchart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UponchartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UponchartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/costardstudio/uponchart/UponchartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.costardstudio.uponchart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UponchartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
