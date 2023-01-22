19f4aa05684a01747ee9310c8f771d34aea90bf6
Valentin Magne <valentin.magne@orange.com>
2023-01-22 14:42:06 +0100

    Updated database with authority and password

--- src/main/java/com/costardstudio/uponchart/entity/UserEntity.java
+++ src/main/java/com/costardstudio/uponchart/entity/UserEntity.java
@@ -1,10 +1,13 @@
 package com.costardstudio.uponchart.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import org.hibernate.annotations.BatchSize;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
 @Entity
@@ -15,6 +18,22 @@ public class UserEntity {
     @GeneratedValue
     private UUID id;
 
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "uc_user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name", referencedColumnName = "name")}
+    )
+    @BatchSize(size = 20)
+    private Set<AuthorityEntity> authorities = new HashSet<>();
+
+    @JsonIgnore
+    @NotNull
+    @Size(min = 60, max = 60)
+    @Column(length = 60, nullable = false)
+    private String password;
+
     private String login;
 
     public UUID getId() {
@@ -23,6 +42,22 @@ public class UserEntity {
 
     public void setId(UUID id) {
         this.id = id;
+    }
+
+    public Set<AuthorityEntity> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Set<AuthorityEntity> authorities) {
+        this.authorities = authorities;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getLogin() {
